cd0ebaa52cd764eb944398b6b545a8e5239cb396
Fabio Naranjo <magnarx99@gmail.com>
2026-05-06 18:57:33 -0500

    codigos pendientes

---
+++ Promedio_de_materia.py
@@ -0,0 +1,15 @@
+Asignatura=str(input("Ingrese asignatura"))
+
+Nota1de4=int(input("Nota1de4")) #
+Nota2de4=int(input("Nota2de4"))
+Nota3de4=int(input("Nota3de4"))
+Nota4de4=int(input("Nota4de4"))
+
+print("Su asignatura es: ", Asignatura)
+print("Su primer nota es: ",  Nota1de4)
+print("Su segunda nota es: ", Nota2de4)
+print("Su tercer nota es: ",  Nota3de4)
+print("Su cuarta nota es: ",  Nota4de4)
+
+Promedio_notas = (Nota1de4 + Nota2de4 + Nota3de4 + Nota4de4) / 4
+print("El promedio de las notas es:", Promedio_notas)
