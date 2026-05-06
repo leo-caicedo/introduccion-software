5792ba8468ff898c7f16d9db92251fdf49582ac5
Fabio Naranjo <magnarx99@gmail.com>
2026-05-06 18:35:50 -0500

    Se agg el codigo de edades de millonarios

---
+++ Edades_de_millonarios.py
@@ -0,0 +1,8 @@
+# Promedio de edades de Millonarios
+
+Bill_Gates = (70)
+Elon_Musk = (54)
+Jeff_Bezos = (62)
+
+Promedio_de_edades_de_millonarios= (Bill_Gates + Elon_Musk + Jeff_Bezos) / 3
+print("El promedio de edades de Millonarios es: ", Promedio_de_edades_de_millonarios)
