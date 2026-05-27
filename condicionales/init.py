d0facfb335b776fff9d59b3ea7a27a6789cd212b
Fabio Naranjo <magnarx99@gmail.com>
2026-05-27 18:22:59 -0500

    Ejercicio condicional simple 1

--- condicionales/init.py
+++ condicionales/init.py
@@ -1,1 +1,11 @@
 # condicionales
+# Construir un código que permita ingresar
+# un número, si el número es mayor de 500, se debe
+# calcular y mostrar en pantalla el 18% de este.
+
+numero = int(input('Ingrese un número: '))
+if numero > 500:
+    porcentaje = numero * 0.18
+    print("El 18% de este número es", porcentaje)
+else:
+    print("Ingrese otro número")
